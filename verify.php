a226f0a9233a318b4c068e57b1800b315350894c
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-05 18:15:55 +0530

    ' bug solved

--- verify.php
+++ verify.php
@@ -85,7 +85,7 @@ Confession Verification page
   function test_input($data) 
     {
         $data = trim($data);
-        $data = stripslashes($data);
+        //$data = stripslashes($data);
         $data = htmlspecialchars($data);
         //fixed a bug which was cousing "rn" to appear whenever "enter" key is pressed
         $data = preg_replace( "/rn/", " ", $data );
