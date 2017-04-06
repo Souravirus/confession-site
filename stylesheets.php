db7c553e33c7b77c8bfe3d19182e10bddfec03ad
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-06 13:05:03 +0530

    stylesheets added

--- stylesheets.php
+++ stylesheets.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" type="text/css" href="css/style1.css" />
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome-animation.css">
 
     <!-- Javascripts -->
     <script src="js/jquery.min.js"></script>
